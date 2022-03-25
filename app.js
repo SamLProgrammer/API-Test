ee3dee253dd832b51651b48297cc7598e04a9dae
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-25 10:21:12 -0500

    cors policy added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 const axios = require('axios')
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
 
