5171c674eeeb9d59048c948e6ab58f841d43d55c
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-24 23:22:52 -0500

    first push

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const port = 5000;
+const axios = require('axios')
+const app = express();
+
+app.get('/', (req, res) => {
+
+    const json_object = { user: 'user', password: '123' }
+
+    axios.post('https://job-manager-xb5oq.ondigitalocean.app/auth/login', json_object)
+        .then((response) => {
+
+            axios.get('https://job-manager-xb5oq.ondigitalocean.app/auth/check-auth', {
+                headers: {
+                    Authorization: response.data.accessToken
+                }
+            }).then((nested_response) => {
+                console.log(nested_response.data)
+            })
+
+        }, (error) => {
+            console.log(error);
+        });
+
+})
+
+app.listen(port, () => {
+    console.log('App Listening on Port ', port);
+})
