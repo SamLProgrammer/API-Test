17fe32e5bf60609ea773bdffaa0b8c957702260d
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-25 10:07:22 -0500

    trying with axios

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
-//const axios = require('axios')
+const axios = require('axios')
 const app = express();
 
 app.get('/', (req, res) => {
 
     const json_object = { user: 'user', password: '123' }
     res.send('ola morzita')
-    // axios.post('https://job-manager-xb5oq.ondigitalocean.app/auth/login', json_object)
-    //     .then((response) => {
+    axios.post('https://job-manager-xb5oq.ondigitalocean.app/auth/login', json_object)
+        .then((response) => {
 
-    //         axios.get('https://job-manager-xb5oq.ondigitalocean.app/auth/check-auth', {
-    //             headers: {
-    //                 Authorization: response.data.accessToken
-    //             }
-    //         }).then((nested_response) => {
-    //             console.log(nested_response.data)
-    //             res.send(nested_response)
-    //         })
+            axios.get('https://job-manager-xb5oq.ondigitalocean.app/auth/check-auth', {
+                headers: {
+                    Authorization: response.data.accessToken
+                }
+            }).then((nested_response) => {
+                console.log(nested_response.data)
+                res.send(nested_response)
+            })
 
-    //     }, (error) => {
-    //         console.log(error);
-    //     });
+        }, (error) => {
+            console.log(error);
+        });
 
 })
 
