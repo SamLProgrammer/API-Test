9c8f6b3b42ef0ade586f43af046b31c7782d821d
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-25 11:02:54 -0500

    nested_response to nested_response. data sent through res

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
                 }
             }).then((nested_response) => {
                 console.log(nested_response.data)
-                res.send(nested_response)
+                res.send(nested_response.data)
             })
 
         }, (error) => {
