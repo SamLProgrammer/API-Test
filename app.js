71c167b00e4db457776f7aea08fd602509db16a9
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-25 09:59:22 -0500

    commenting axios

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
-const axios = require('axios')
+const port = process.env.PORT || 5000;
+//const axios = require('axios')
 const app = express();
 
 app.get('/', (req, res) => {
 
-    req.setTimeout(100000);
     const json_object = { user: 'user', password: '123' }
+    res.send(json_object)
+    // axios.post('https://job-manager-xb5oq.ondigitalocean.app/auth/login', json_object)
+    //     .then((response) => {
 
-    axios.post('https://job-manager-xb5oq.ondigitalocean.app/auth/login', json_object)
-        .then((response) => {
+    //         axios.get('https://job-manager-xb5oq.ondigitalocean.app/auth/check-auth', {
+    //             headers: {
+    //                 Authorization: response.data.accessToken
+    //             }
+    //         }).then((nested_response) => {
+    //             console.log(nested_response.data)
+    //             res.send(nested_response)
+    //         })
 
-            axios.get('https://job-manager-xb5oq.ondigitalocean.app/auth/check-auth', {
-                headers: {
-                    Authorization: response.data.accessToken
-                }
-            }).then((nested_response) => {
-                console.log(nested_response.data)
-                res.send(nested_response)
-            })
-
-        }, (error) => {
-            console.log(error);
-        });
+    //     }, (error) => {
+    //         console.log(error);
+    //     });
 
 })
 
