01a8253074993f65c18d7405045b96fad974e36a
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-25 10:02:32 -0500

    saludando al  poio

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
 
     const json_object = { user: 'user', password: '123' }
-    res.send(json_object)
+    res.send('ola morzita')
     // axios.post('https://job-manager-xb5oq.ondigitalocean.app/auth/login', json_object)
     //     .then((response) => {
 
